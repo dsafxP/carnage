c70a7ceefed6d5fdd9cb7116620523b9090f778c
dsaf <ghostyn678+git@gmail.com>
2026-02-17 22:48:29 -0300

    feat(main): add script clause
    needed for testing with system portage

--- carnage/__init__.py
+++ carnage/__init__.py
@@ -1,5 +1,3 @@
 """TUI front-end for Portage and eix"""
 
 __version__ = "1.2b"
-
-from carnage.main import main
--- carnage/main.py
+++ carnage/main.py
@@ -3,3 +3,6 @@ from carnage.tui.app import run
 
 def main() -> None:
     run()
+
+if __name__ == "__main__":
+    main()
