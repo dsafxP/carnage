405c47b91b2fb405187c68d551e57445a017680e
dsaf <ghostyn678+git@gmail.com>
2026-04-19 21:47:20 -0300

    feat: root safeguard

--- carnage/main.py
+++ carnage/main.py
@@ -1,17 +1,22 @@
+import os
+import sys
+
 from carnage.core.lock import InstanceLock
 from carnage.tui.app import CarnageApp
 
 
 def main() -> None:
+    # Check if running as root
+    if os.geteuid() == 0:
+        print("carnage must not be run as root!")
+        sys.exit(1)
+
     app = CarnageApp()
 
     lock = InstanceLock(on_signal=lambda _: app.bell())
 
     if not lock.acquire():
-        import sys
-
         sys.exit(0)
-
     try:
         app.run()
     finally:
